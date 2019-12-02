63ee2164d11319f093f9314535b2a762f4ef0bc6
rainierrr <b1916320@planet.kanazawa-it.ac.jp>
2019-12-03 01:26:15 +0900

    ベースコード完成

---
+++ main.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import sys
+
+
+def headers_gengerator(df):                      #headersの作成
+    headers = df.columns
+    with open("headers.txt", mode="w") as f:
+        f.write("HEADERS = [\n")
+        for s in headers:
+            f.write('   "' + s + '",\n')
+        f.write("].freeze\n")
+    return headers
+
+
+def columns_gengerator(df, headers):                     #カラムの作成
+    columns = []
+
+    for i in range(65, 65+26):                  #AA~ZZまでのリストを作成
+        for ii in range(65, 65+26):             #数字をchr()でasciiコードから文字へ変換している
+            if i == 65:
+                columns.append(chr(ii))
+            else:
+                columns.append(chr(i)+chr(ii))
+
+    colmuns = columns[0:len(df.columns)]        #CSVのヘッダーの数だけのカラムを取り出している
+    numbers = list(df.loc[2])                   #サンプル用の値
+
+    with open("colmuns.txt", mode="w") as f:
+        f.write("csv << [\n")
+        for colmun, header, number in zip(columns, headers, numbers):
+            if number == None:
+                f.write("nil,       ")
+            elif number == 0:
+                f.write('"0",       ')
+            else:
+                f.write(",        ")
+            f.write("#列" + colmun + " " + header + "\n")
+        f.write("]\n")
+
+    return  colmuns
+
+
+
+args = sys.argv #コマンドライン引数　ファイル名を入れる
+df = pd.read_csv(args[1], encoding="utf-8")
+
+headers = headers_gengerator(df)
+columns_gengerator(df, headers)
+
+
+
+
+
+
+
+
+
